30549d74f9996f95108b93b4d2b523a301d2ef91
Stefan Marr <git@stefan-marr.de>
2024-03-23 16:15:20 +0000

    Replace tslint with eslint and prettier and format code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/backgroundColourPlugin.ts
+++ src/backgroundColourPlugin.ts
@@ -1,16 +1,16 @@
 import { Chart as ChartJS, Plugin as ChartJSPlugin } from 'chart.js';
 
 export class BackgroundColourPlugin implements ChartJSPlugin {
-	public readonly id: string = 'chartjs-plugin-chartjs-node-canvas-background-colour';
+	public readonly id: string =
+		'chartjs-plugin-chartjs-node-canvas-background-colour';
 
 	public constructor(
 		private readonly _width: number,
 		private readonly _height: number,
 		private readonly _fillStyle: string
-	) { }
+	) {}
 
 	public beforeDraw(chart: ChartJS): boolean | void {
-
 		const ctx = chart.ctx;
 		ctx.save();
 		ctx.fillStyle = this._fillStyle;
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
 // syntax:
 // declare module '<module>';
 
+// eslint-disable-next-line max-len
 declare module 'resemblejs'; // Has a 'declare global' that causes typescript to add  a '/// <reference types="resemblejs" />'
